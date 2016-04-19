ce57afb321116b0537541c03236547f08595198f
watanabe <watanabe.seman@gmail.com>
2016-04-20 01:23:06 +0900

    first commit

---
+++ babel-react-es2015/src.jsx
@@ -0,0 +1,5 @@
+class Hello extends React {
+    render() {
+        return <div>Hello world</div>
+    }
+}
---
+++ babel-react/bundle.js
@@ -0,0 +1,11 @@
+var Hello = React.createClass({
+    displayName: "Hello",
+
+    render: function () {
+        return React.createElement(
+            "div",
+            null,
+            "Hello world"
+        );
+    }
+});
