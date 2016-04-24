11e9440d853b2dbcc4dc3385d156d04c213ae5d2
watanabe <watanabe.seman@gmail.com>
2016-04-24 16:16:25 +0900

    webpack redux

--- webpack-react/js/src.jsx
+++ webpack-react/js/src.jsx
@@ -1,4 +1,4 @@
-const React = require('react');
+const React = require('react'); // Must require react because <Hello/> is translated to React.createElement(Hello).
 const ReactDOM = require('react-dom');
 const Hello = require('./lib.jsx');
 
