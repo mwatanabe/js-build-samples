d72d160a27646864a1e6c0cdf50a2958f4e205b8
watanabe <watanabe.seman@gmail.com>
2016-04-24 15:25:41 +0900

    webpack react

---
+++ webpack-react/js/lib.jsx
@@ -0,0 +1,9 @@
+const React = require('react');
+
+class Hello extends React.Component {
+    render() {
+        return <div>Hello world!!!!!</div>
+    }
+}
+
+module.exports = Hello;
---
+++ webpack-react/js/src.jsx
@@ -0,0 +1,5 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+const Hello = require('./lib.jsx');
+
+ReactDOM.render(<Hello />, document.body);
